fc31947a8dc30cc13b2de3d559504b48bf6ed80d
Ritik Joshi <ritik.joshi329@gmail.com>
2023-10-16 22:51:25 +0530

    first

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ connectDB()
 
 app.use(logger)
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use(cors(corsOptions))
 
 app.use(express.json())
